8361de831a0d3778a0f36b54ed1dfd3bab39f5a9
parksangseok <park0157@naver.com>
2025-07-24 18:47:07 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function () {
 
+  //aos 초기화
+  $(window).load(function () {
+    AOS.init({
+      duration: 1300
+    });
+  });
+
   $("#liveClassWrap .slick").slick({
     variableWidth: true,
     autoplay: true,
