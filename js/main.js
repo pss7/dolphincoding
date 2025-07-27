bd60dcd542550a7649069cb743de96b7c6e4ac2d
parksangseok <park0157@naver.com>
2025-07-27 14:56:34 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -7,41 +7,51 @@ $(function () {
     });
   });
 
-  $("#liveClassWrap .slick").slick({
-    variableWidth: true,
-    autoplay: true,
-    arrows: false,
-    dots: false,
-    accessibility: false,
-    draggable: true,
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    zIndex: 1000,
-    pauseOnHover: false,
-    autoplaySpeed: 5000,
-    speed: 1500,
-    centerMode: true,
-    centerPadding: '0',
+  // $("#liveClassWrap .slick").slick({
+  //   variableWidth: true,
+  //   autoplay: true,
+  //   arrows: false,
+  //   dots: false,
+  //   accessibility: false,
+  //   draggable: true,
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   zIndex: 1000,
+  //   pauseOnHover: false,
+  //   autoplaySpeed: 5000,
+  //   speed: 1500,
+  //   centerMode: true,
+  //   centerPadding: '0',
+  // });
+
+  // function updateSlideSizes() {
+  //   const $slides = $('#liveClassWrap .liveClassSlideBox .slide');
+  //   $slides.removeClass('near-center');
+
+  //   const $center = $('#liveClassWrap .liveClassSlideBox .slick-center');
+  //   const $prev = $center.prev('.slide');
+  //   const $next = $center.next('.slide');
+
+  //   $prev.addClass('near-center');
+  //   $next.addClass('near-center');
+  // }
+
+  // $('#liveClassWrap .slick').on('setPosition', function () {
+  //   updateSlideSizes();
+  // });
+
+
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
   });
 
-  function updateSlideSizes() {
-    const $slides = $('#liveClassWrap .liveClassSlideBox .slide');
-    $slides.removeClass('near-center');
 
-    const $center = $('#liveClassWrap .liveClassSlideBox .slick-center');
-    const $prev = $center.prev('.slide');
-    const $next = $center.next('.slide');
 
-    $prev.addClass('near-center');
-    $next.addClass('near-center');
-  }
-
-  $('#liveClassWrap .slick').on('setPosition', function () {
-    updateSlideSizes();
-  });
-
- let slickApplied = false;
+  let slickApplied = false;
 
   function initOrDestroySlick() {
     const $slider = $("#popularWrap .slick");
